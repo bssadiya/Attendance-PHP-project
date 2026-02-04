b7883fb077d0779eb3147600303447855b286c93
bssadiya <156563575+bssadiya@users.noreply.github.com>
2026-02-05 01:30:20 +0530

    Update classTeacherLogin.php

--- classTeacherLogin.php
+++ classTeacherLogin.php
@@ -1,116 +1,181 @@
-
-<?php 
+<?php
 include 'Includes/dbcon.php';
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
   <link href="img/logo/attnlg.jpg" rel="icon">
-  <title>AMS - Login</title>
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-  <link href="css/ruang-admin.min.css" rel="stylesheet">
+  <title>AMS - Class Teacher Login</title>
 
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/ruang-admin.min.css" rel="stylesheet">
 </head>
 
 <body class="bg-gradient-login">
-  <!-- Login Content -->
-  <div class="container-login">
-    <div class="row justify-content-center">
-      <div class="col-xl-10 col-lg-12 col-md-9">
-        <div class="card shadow-sm my-5">
-          <div class="card-body p-0">
-            <div class="row">
-              <div class="col-lg-12">
-                <div class="login-form">
-                  <div class="text-center">
-                    <img src="img/logo/attnlg.jpg" style="width:100px;height:100px">
-                    <br><br>
-                    <h1 class="h4 text-gray-900 mb-4">Login</h1>
+
+<div class="container-login">
+  <div class="row justify-content-center">
+    <div class="col-xl-10 col-lg-12 col-md-9">
+      <div class="card shadow-sm my-5">
+        <div class="card-body p-0">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="login-form">
+
+                <div class="text-center">
+                  <img src="img/logo/attnlg.jpg" style="width:100px;height:100px">
+                  <br><br>
+                  <h1 class="h4 text-gray-900 mb-4">Class Teacher Login</h1>
+                </div>
+
+                <form method="POST">
+                  <div class="form-group">
+                    <input type="email" class="form-control" required
+                           name="username" placeholder="Email Address">
                   </div>
-                  <form class="user" method="Post" action="">
-                    <div class="form-group">
-                      <input type="text" class="form-control" required name="username" id="exampleInputEmail" placeholder="Enter Email Address">
-                    </div>
-                    <div class="form-group">
-                      <input type="password" name = "password" required class="form-control" id="exampleInputPassword" placeholder="Enter Password">
-                    </div>
-                    <div class="form-group">
-                      <div class="custom-control custom-checkbox small" style="line-height: 1.5rem;">
-                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                        <!-- <label class="custom-control-label" for="customCheck">Remember
-                          Me</label> -->
-                      </div>
-                    </div>
-                    <div class="form-group">
-                        <input type="submit"  class="btn btn-primary btn-block" value="Login" name="login" />
-                    </div>
-                     </form>
+
+                  <div class="form-group">
+                    <input type="password" class="form-control" required
+                           name="password" placeholder="Password">
+                  </div>
+
+                  <div class="form-group">
+                    <button type="submit" name="login"
+                            class="btn btn-primary btn-block">
+                      Login
+                    </button>
+                  </div>
+                </form>
 
 <?php
+if (isset($_POST['login'])) {
 
-  if(isset($_POST['login'])){
+  $email = trim($_POST['username']);
+  $password = $_POST['password'];
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+  $stmt = $conn->prepare(
+    "SELECT Id, firstName, lastName, emailAddress, password,
+            classId, classArmId,
+            status, failed_attempts
+     FROM tblclassteacher
+     WHERE emailAddress = ?"
+  );
+  $stmt->bind_param("s", $email);
+  $stmt->execute();
+  $result = $stmt->get_result();
 
-    $password = md5($password);
-
-    $query = "SELECT * FROM tblclassteacher WHERE emailAddress = '$username' AND password = '$password'";
-    $rs = $conn->query($query);
-    $num = $rs->num_rows;
-    $rows = $rs->fetch_assoc();
-
-    if($num > 0){
-
-      $_SESSION['userId'] = $rows['Id'];
-      $_SESSION['firstName'] = $rows['firstName'];
-      $_SESSION['lastName'] = $rows['lastName'];
-      $_SESSION['emailAddress'] = $rows['emailAddress'];
-      $_SESSION['classId'] = $rows['classId'];
-      $_SESSION['classArmId'] = $rows['classArmId'];
-
-      echo "<script type = \"text/javascript\">
-      window.location = (\"ClassTeacher/index.php\")
-      </script>";
-    }
-
-    else{
-
-      echo "<div class='alert alert-danger' role='alert'>
-      Invalid Username/Password!
-      </div>";
-
-    }
+  if ($result->num_rows !== 1) {
+    echo "<div class='alert alert-danger'>Invalid email or password.</div>";
+    exit();
   }
+
+  $user = $result->fetch_assoc();
+
+  /* 🔒 ACCOUNT BLOCKED */
+  if ($user['status'] === 'blocked') {
+    echo "<div class='alert alert-danger'>
+            Account locked due to multiple failed attempts.<br>
+            Please check your email to unlock.
+          </div>";
+    exit();
+  }
+
+  /* ✅ PASSWORD CORRECT */
+  if (password_verify($password, $user['password'])) {
+
+    // reset failed attempts
+    $reset = $conn->prepare(
+      "UPDATE tblclassteacher
+       SET failed_attempts = 0
+       WHERE Id = ?"
+    );
+    $reset->bind_param("i", $user['Id']);
+    $reset->execute();
+
+    session_regenerate_id(true);
+
+    $_SESSION['userId']     = $user['Id'];
+    $_SESSION['firstName']  = $user['firstName'];
+    $_SESSION['lastName']   = $user['lastName'];
+    $_SESSION['emailAddress'] = $user['emailAddress'];
+    $_SESSION['classId']    = $user['classId'];
+    $_SESSION['classArmId'] = $user['classArmId'];
+
+    echo "<script>window.location='ClassTeacher/index.php'</script>";
+    exit();
+  }
+
+  /* ❌ PASSWORD WRONG */
+  $attempts = $user['failed_attempts'] + 1;
+
+  /* 🚨 LOCK ACCOUNT */
+  if ($attempts >= 5) {
+
+    $token  = bin2hex(random_bytes(32));
+    $expiry = date("Y-m-d H:i:s", strtotime("+30 minutes"));
+
+    $lock = $conn->prepare(
+      "UPDATE tblclassteacher
+       SET failed_attempts = ?,
+           status = 'blocked',
+           unlock_token = ?,
+           token_expiry = ?
+       WHERE Id = ?"
+    );
+    $lock->bind_param("issi", $attempts, $token, $expiry, $user['Id']);
+    $lock->execute();
+
+    /* 📧 SEND UNLOCK EMAIL */
+    $unlockLink = "http://localhost/attendance-php/unlockByEmail.php?token=$token";
+
+    $subject = "Account Locked - Unlock Required";
+    $message = "Your account has been locked after multiple failed login attempts.\n\n";
+    $message .= "Click the link below to unlock your account:\n";
+    $message .= $unlockLink . "\n\n";
+    $message .= "This link will expire in 30 minutes.";
+
+    $headers = "From: no-reply@attendance-system.com";
+
+    mail($user['emailAddress'], $subject, $message, $headers);
+
+    echo "<div class='alert alert-danger'>
+            Account locked after 5 failed attempts.<br>
+            Unlock link sent to your email.
+          </div>";
+    exit();
+  }
+
+  /* ⏳ STILL ALLOWED */
+  $upd = $conn->prepare(
+    "UPDATE tblclassteacher
+     SET failed_attempts = ?
+     WHERE Id = ?"
+  );
+  $upd->bind_param("ii", $attempts, $user['Id']);
+  $upd->execute();
+
+  echo "<div class='alert alert-danger'>
+          Invalid password. Attempt $attempts of 5.
+        </div>";
+}
 ?>
 
-                    <!-- <hr>
-                    <a href="index.html" class="btn btn-google btn-block">
-                      <i class="fab fa-google fa-fw"></i> Login with Google
-                    </a>
-                    <a href="index.html" class="btn btn-facebook btn-block">
-                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                    </a> -->
-                  <hr>
-                  <div class="text-center">
-                    <a class="font-weight-bold small" href="classTeacherLogin.php">Class Teacher Login!</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <!-- <a class="font-weight-bold small" href=".php">Cooperative Account!</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <a class="font-weight-bold small" href="forgotPassword.php">Forgot Password?</a> -->
-
-                  </div>
-                  <div class="text-center">
-                  </div>
+                <hr>
+                <div class="text-center">
+                  <a class="font-weight-bold small" href="forgotPassword.php">
+                    Forgot Password?
+                  </a>
                 </div>
+
               </div>
             </div>
           </div>
@@ -118,11 +183,10 @@ session_start();
       </div>
     </div>
   </div>
-  <!-- Login Content -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="js/ruang-admin.min.js"></script>
-</body>
+</div>
 
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="js/ruang-admin.min.js"></script>
+</body>
 </html>
